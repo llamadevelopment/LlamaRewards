66b6edbd2fcb423ade93e4224f719a2c485b345a
Mundschutziii <jop.pretzer@gmx.de>
2021-01-03 19:20:00 +0100

    fixed mysql issue

--- src/main/java/net/lldv/llamarewards/components/provider/MySqlProvider.java
+++ src/main/java/net/lldv/llamarewards/components/provider/MySqlProvider.java
@@ -2,28 +2,36 @@ package net.lldv.llamarewards.components.provider;
 
 import cn.nukkit.Player;
 import cn.nukkit.Server;
-import cn.nukkit.utils.Config;
 import net.lldv.llamarewards.LlamaRewards;
 import net.lldv.llamarewards.components.data.Reward;
+import net.lldv.llamarewards.components.simplesqlclient.MySqlClient;
+import net.lldv.llamarewards.components.simplesqlclient.objects.SqlColumn;
+import net.lldv.llamarewards.components.simplesqlclient.objects.SqlDocument;
 
-import java.sql.*;
 import java.util.Arrays;
-import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 public class MySqlProvider extends Provider {
 
-    private Connection connection;
+    private MySqlClient client;
 
     @Override
     public void connect(LlamaRewards instance) {
         CompletableFuture.runAsync(() -> {
             try {
-                Config config = instance.getConfig();
-                Class.forName("com.mysql.jdbc.Driver");
-                this.connection = DriverManager.getConnection("jdbc:mysql://" + config.getString("MySql.Host") + ":" + config.getString("MySql.Port") + "/" + config.getString("MySql.Database") + "?autoReconnect=true&useGmtMillisForDatetimes=true&serverTimezone=GMT", config.getString("MySql.User"), config.getString("MySql.Password"));
-                this.update("CREATE TABLE IF NOT EXISTS reward_data(player VARCHAR(50), rewards LONGTEXT, PRIMARY KEY (player));");
+                this.client = new MySqlClient(
+                        instance.getConfig().getString("MySql.Host"),
+                        instance.getConfig().getString("MySql.Port"),
+                        instance.getConfig().getString("MySql.User"),
+                        instance.getConfig().getString("MySql.Password"),
+                        instance.getConfig().getString("MySql.Database")
+                );
+
+                this.client.createTable("reward_data", "player",
+                        new SqlColumn("player", SqlColumn.Type.VARCHAR, 50)
+                                .append("rewards", SqlColumn.Type.LONGTEXT));
+
                 instance.getLogger().info("[MySqlClient] Connection opened.");
             } catch (Exception e) {
                 e.printStackTrace();
@@ -32,110 +40,75 @@ public class MySqlProvider extends Provider {
         });
     }
 
-    public void update(String query) {
-        CompletableFuture.runAsync(() -> {
-            if (this.connection != null) {
-                try {
-                    PreparedStatement preparedStatement = this.connection.prepareStatement(query);
-                    preparedStatement.executeUpdate();
-                    preparedStatement.close();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-    }
-
     @Override
     public void disconnect(LlamaRewards instance) {
-        if (this.connection != null) {
-            try {
-                this.connection.close();
-                instance.getLogger().info("[MySqlClient] Connection closed.");
-            } catch (SQLException throwables) {
-                throwables.printStackTrace();
-                instance.getLogger().info("[MySqlClient] Failed to close connection.");
-            }
-        }
+        instance.getLogger().info("[MySqlClient] Connection closed.");
     }
 
     @Override
     public boolean playerExists(Player player) {
-        try {
-            PreparedStatement preparedStatement = this.connection.prepareStatement("SELECT * FROM reward_data WHERE PLAYER = ?");
-            preparedStatement.setString(1, player.getName());
-            ResultSet resultSet = preparedStatement.executeQuery();
-            if (resultSet.next()) return resultSet.getString("PLAYER") != null;
-            resultSet.close();
-            preparedStatement.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return false;
+        SqlDocument document = this.client.find("reward_data", "player", player.getName()).first();
+        return document != null;
     }
 
     @Override
     public void createPlayer(Player player) {
-        this.update("INSERT INTO reward_data (PLAYER, REWARDS) VALUES ('" + player.getName() + "', '');");
+        CompletableFuture.runAsync(() -> this.client.insert("reward_data", new SqlDocument("player", player.getName()).append("rewards", "")));
     }
 
     @Override
     public void redeemReward(Player player, Reward reward) {
         long intervalSet = (reward.getInterval() * 3600000L) + System.currentTimeMillis();
-        String rawList = "";
-        try {
-            PreparedStatement preparedStatement = this.connection.prepareStatement("SELECT * FROM reward_data WHERE PLAYER = ?");
-            preparedStatement.setString(1, player.getName());
-            ResultSet resultSet = preparedStatement.executeQuery();
-            if (resultSet.next()) rawList = resultSet.getString("REWARDS");
-            resultSet.close();
-            preparedStatement.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+
+        SqlDocument document = this.client.find("reward_data", "player", player.getName()).first();
+        String rawList = document.getString("rewards");
         List<String> list = Arrays.asList(rawList.split(";-;"));
-        String[] listSet = rawList.split(";-;");
-        Iterator<String> iterator = list.iterator();
-        while (iterator.hasNext()) {
-            String data = iterator.next();
-            String[] dataString = data.split("##");
-            if (dataString[0].equals(reward.getName())) {
-                iterator.remove();
+
+        if (reward.equals("") || rawList.isEmpty()) {
+            this.client.update("reward_data", "player", player.getName(), new SqlDocument("rewards", reward.getName() + "##" + intervalSet + ";-;"));
+            reward.getRewards().forEach(command -> Server.getInstance().dispatchCommand(Server.getInstance().getConsoleSender(), command.replace("%p", player.getName())));
+            player.sendMessage(reward.getMessage());
+            return;
+        }
+
+        String[] s = document.getString("rewards").split(";-;");
+        String ignore = "";
+
+        for (String e : s) {
+            if (e.startsWith(reward.getName())) {
+                ignore = reward.getName() + "##" + e.split("##")[1];
             }
         }
+
         StringBuilder set = new StringBuilder();
-        for (String s : listSet) {
-            set.append(s).append(";-;");
+        for (String e : list) {
+            if (!e.equals(ignore)) {
+                set.append(e).append(";-;");
+            }
         }
-        this.update("UPDATE reward_data SET REWARDS= '" + set + reward.getName() + "##" + intervalSet + "' WHERE PLAYER= '" + player.getName() + "';");
+
+        this.client.update("reward_data", "player", player.getName(), new SqlDocument("rewards", set.toString() + reward.getName() + "##" + intervalSet + ";-;"));
         reward.getRewards().forEach(command -> Server.getInstance().dispatchCommand(Server.getInstance().getConsoleSender(), command.replace("%p", player.getName())));
         player.sendMessage(reward.getMessage());
     }
 
     @Override
     public boolean canRedeem(Player player, Reward reward) {
-        String rawList = "";
-        try {
-            PreparedStatement preparedStatement = this.connection.prepareStatement("SELECT * FROM reward_data WHERE PLAYER = ?");
-            preparedStatement.setString(1, player.getName());
-            ResultSet resultSet = preparedStatement.executeQuery();
-            if (resultSet.next()) rawList = resultSet.getString("REWARDS");
-            resultSet.close();
-            preparedStatement.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        List<String> list = Arrays.asList(rawList.split(";-;"));
-        if (list.size() == 0) return true;
-
-        boolean value = true;
-        for (String data : list) {
-            String[] dataString = data.split("##");
-            if (dataString[0].equals(reward.getName())) {
-                value = System.currentTimeMillis() >= Long.parseLong(dataString[1]);
+        SqlDocument document = this.client.find("reward_data", "player", player.getName()).first();
+        if (document != null) {
+            String rawList = document.getString("rewards");
+            List<String> list = Arrays.asList(rawList.split(";-;"));
+            if (list.size() == 0) return true;
+            boolean value = true;
+            for (String data : list) {
+                String[] dataString = data.split("##");
+                if (dataString[0].equals(reward.getName())) {
+                    value = System.currentTimeMillis() >= Long.parseLong(dataString[1]);
+                }
             }
+            return value;
         }
-        return value;
+        return false;
     }
 
     @Override
